ab1aadd2cdcb16d3a7c78c2245f780840a9b129b
kushchandna <kushchandna@KUSH>
2017-11-06 07:49:40 +0530

    completed sample client method

--- service/src/test/java/com/kush/lib/service/sample/client/SampleApplicationClient.java
+++ service/src/test/java/com/kush/lib/service/sample/client/SampleApplicationClient.java
@@ -3,6 +3,8 @@ package com.kush.lib.service.sample.client;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.kush.lib.service.api.client.Response;
+import com.kush.lib.service.api.client.Response.ResultListener;
 import com.kush.lib.service.sample.server.SampleApplicationServer;
 import com.kush.lib.service.sample.server.SampleService;
 
@@ -22,7 +24,14 @@ public class SampleApplicationClient {
 
     public void invokeGetHelloText() {
         SampleServiceClient sampleServiceClient = new SampleServiceClient(getSampleService(), executor);
-        sampleServiceClient.getHelloText("TestUser");
+        Response<String> response = sampleServiceClient.getHelloText("TestUser");
+        response.setResultListener(new ResultListener<String>() {
+
+            @Override
+            public void onResult(String result) {
+                System.out.println(result);
+            }
+        });
     }
 
     private SampleService getSampleService() {
