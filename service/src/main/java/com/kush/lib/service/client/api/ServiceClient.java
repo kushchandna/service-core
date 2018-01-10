da48ebe49a169d7cbdc955658222122da4f9c520
Kush <Kush@KUSH>
2018-01-11 02:09:51 +0530

    moved more part to super service client class

--- service/src/main/java/com/kush/lib/service/client/api/ServiceClient.java
+++ service/src/main/java/com/kush/lib/service/client/api/ServiceClient.java
@@ -5,12 +5,27 @@ import java.util.concurrent.Executor;
 public abstract class ServiceClient {
 
     private final Responder responder;
+    private final ServiceInvoker serviceInvoker;
+    private final String serviceName;
 
-    public ServiceClient(Executor executor) {
+    public ServiceClient(Executor executor, ServiceInvoker serviceInvoker, String serviceName) {
+        this.serviceInvoker = serviceInvoker;
+        this.serviceName = serviceName;
         responder = new Responder(executor);
     }
 
-    protected <T> Response<T> invoke(ServiceTask<T> task) {
-        return responder.invoke(task);
+    protected final <T> Response<T> invoke(Class<T> returnType, String methodName, Object... arguments) {
+        ServiceTask<T> serviceTask = createServiceTask(returnType, methodName, arguments);
+        return responder.invoke(serviceTask);
+    }
+
+    private <T> ServiceTask<T> createServiceTask(Class<T> returnType, String methodName, Object... arguments) {
+        return new ServiceTask<T>() {
+
+            @Override
+            public T execute() throws ServiceFailedException {
+                return returnType.cast(serviceInvoker.invoke(serviceName, methodName, arguments));
+            }
+        };
     }
 }
--- service/src/test/java/com/kush/lib/service/sample/client/SampleServiceClient.java
+++ service/src/test/java/com/kush/lib/service/sample/client/SampleServiceClient.java
@@ -4,26 +4,15 @@ import java.util.concurrent.Executor;
 
 import com.kush.lib.service.client.api.Response;
 import com.kush.lib.service.client.api.ServiceClient;
-import com.kush.lib.service.client.api.ServiceFailedException;
 import com.kush.lib.service.client.api.ServiceInvoker;
-import com.kush.lib.service.client.api.ServiceTask;
 
 public class SampleServiceClient extends ServiceClient {
 
-    private final ServiceInvoker serviceInvoker;
-
     public SampleServiceClient(Executor executor, ServiceInvoker serviceInvoker) {
-        super(executor);
-        this.serviceInvoker = serviceInvoker;
+        super(executor, serviceInvoker, "Sample Service");
     }
 
     public Response<String> getHelloText(String name) {
-        return invoke(new ServiceTask<String>() {
-
-            @Override
-            public String execute() throws ServiceFailedException {
-                return (String) serviceInvoker.invoke("Sample Service", "getHelloText");
-            }
-        });
+        return invoke(String.class, "sayHello", name);
     }
 }
