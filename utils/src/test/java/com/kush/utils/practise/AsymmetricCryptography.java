43896177c41ffee8fc44ba7a48c21a174c2b9db7
Kush Chandna <kushchandna@gmail.com>
2018-02-18 04:04:45 +0530

    crypto

---
+++ utils/src/test/java/com/kush/utils/practise/AsymmetricCryptography.java
@@ -0,0 +1,36 @@
+package com.kush.utils.practise;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+
+import javax.crypto.Cipher;
+
+import org.junit.Test;
+
+public class AsymmetricCryptography {
+
+    @Test
+    public void test() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+
+        String text = "kushchandna";
+
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedBytes = cipher.doFinal(text.getBytes());
+
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+
+        assertThat(new String(decryptedBytes), is(equalTo(text)));
+    }
+}
