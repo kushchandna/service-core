6d90b034df1a1c00c764e8df4b58447303ee9695
Kush Chandna <kushchandna@gmail.com>
2018-02-18 02:53:23 +0530

    strings

--- utils/src/main/java/com/kush/utils/practise/Strings.java
+++ utils/src/main/java/com/kush/utils/practise/Strings.java
@@ -1,22 +1,39 @@
 package com.kush.utils.practise;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.Test;
 
 public class Strings {
 
     @Test
-    public void strings() throws Exception {
-
+    public void string1() throws Exception {
         String str1 = "kush";
         String str2 = "kush";
-        String str3 = new String("kush");
-        String str4 = str3.intern();
-        String str5 = new String(str4);
+        assertThat(str1 == str2, is(equalTo(true)));
+    }
 
-        System.out.println(str1 == str2);
-        System.out.println(str2 == str3);
-        System.out.println(str3 == str4);
-        System.out.println(str4 == str1);
-        System.out.println(str5 == str4);
+    @Test
+    public void string2() throws Exception {
+        String str1 = "kush";
+        String str2 = new String("kush");
+        assertThat(str1 == str2, is(equalTo(false)));
+    }
+
+    @Test
+    public void string3() throws Exception {
+        String str1 = "kush";
+        String str3 = new String("kush");
+        String str2 = str3.intern();
+        assertThat(str1 == str2, is(equalTo(true)));
+    }
+
+    @Test
+    public void string4() throws Exception {
+        String str1 = "kush";
+        String str2 = new String(str1);
+        assertThat(str1 == str2, is(equalTo(false)));
     }
 }
