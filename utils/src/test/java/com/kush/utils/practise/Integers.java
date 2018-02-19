b27a36a9acc171c52afcf4358e22c7ef3b9ab59f
Kush Chandna <kushchandna@gmail.com>
2018-02-19 14:49:45 +0530

    test

--- utils/src/test/java/com/kush/utils/practise/Integers.java
+++ utils/src/test/java/com/kush/utils/practise/Integers.java
@@ -24,29 +24,29 @@ public class Integers {
 
     @Test
     public void integer3() throws Exception {
+        Integer int1 = new Integer(20);
+        Integer int2 = 20;
+        assertThat(int1 == int2, is(equalTo(false)));
+    }
+
+    @Test
+    public void integer4() throws Exception {
         Integer int1 = 20;
         int int2 = 20;
         assertThat(int1 == int2, is(equalTo(true)));
     }
 
     @Test
-    public void integer4() throws Exception {
+    public void integer5() throws Exception {
         Integer int1 = 200;
         int int2 = 200;
         assertThat(int1 == int2, is(equalTo(true)));
     }
 
     @Test
-    public void integer5() throws Exception {
+    public void integer6() throws Exception {
         Integer int1 = new Integer(20);
         int int2 = 20;
         assertThat(int1 == int2, is(equalTo(true)));
-    }
-
-    @Test
-    public void integer6() throws Exception {
-        Integer int1 = new Integer(20);
-        Integer int2 = 20;
-        assertThat(int1 == int2, is(equalTo(false)));
     }
 }
