0cc394e2344624cf27bba70d43560a44feac17c6
Kush Chandna <kushchandna@gmail.com>
2018-03-09 13:52:58 +0530

    improved service client generator plugin

--- serviceclient-maven-plugin/src/main/java/com/kush/servicegen/maven/ServiceClientJarGeneratorMojo.java
+++ serviceclient-maven-plugin/src/main/java/com/kush/servicegen/maven/ServiceClientJarGeneratorMojo.java
@@ -8,9 +8,11 @@ import java.util.Collection;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import com.kush.servicegen.CodeGenerationFailedException;
 import com.kush.servicegen.clients.ServiceClientInfo;
@@ -19,21 +21,29 @@ import com.kush.servicegen.clients.ServiceClientJarGenerator;
 @Mojo(name = "generate-service-clients", defaultPhase = LifecyclePhase.COMPILE)
 public class ServiceClientJarGeneratorMojo extends AbstractMojo {
 
-    @Parameter
+    @Component
+    private MavenProject mavenProject;
+
+    @Parameter(defaultValue = "${project.build.directory}", readonly = true)
     private File targetDirectory;
-    @Parameter
-    private String targetJarName;
     @Parameter
     private ServiceClientInfo[] serviceClients;
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        ServiceClientJarGenerator jarGenerator = new ServiceClientJarGenerator(targetDirectory, targetJarName);
+        String serviceClientJarName = prepareServiceClientJarName();
+        ServiceClientJarGenerator jarGenerator = new ServiceClientJarGenerator(targetDirectory, serviceClientJarName);
         Collection<ServiceClientInfo> serviceClientInfos = Arrays.asList(serviceClients);
         try {
             jarGenerator.generate(serviceClientInfos);
         } catch (ClassNotFoundException | CodeGenerationFailedException | IOException e) {
             throw new MojoExecutionException(e.getMessage(), e);
         }
+    }
+
+    private String prepareServiceClientJarName() {
+        String projectName = mavenProject.getName();
+        String projectVersion = mavenProject.getVersion();
+        return projectName + "-serviceclients-" + projectVersion + ".jar";
     }
 }
