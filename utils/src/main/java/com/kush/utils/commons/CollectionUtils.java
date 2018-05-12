a7755d8b7e3a77ca4befbd191e5f5f11064800b5
Kush <Kush@KUSH>
2018-05-12 16:55:49 +0530

    added a utility method

--- utils/src/main/java/com/kush/utils/commons/CollectionUtils.java
+++ utils/src/main/java/com/kush/utils/commons/CollectionUtils.java
@@ -1,7 +1,11 @@
 package com.kush.utils.commons;
 
+import static java.util.Collections.singleton;
+import static java.util.Collections.singletonMap;
+
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class CollectionUtils {
@@ -12,5 +16,9 @@ public class CollectionUtils {
 
     public static <T> Set<T> nonNull(Set<T> set) {
         return set == null ? Collections.emptySet() : set;
+    }
+
+    public static <K, V> Map<K, Set<V>> singletonMultiValueMap(K key, V value) {
+        return singletonMap(key, singleton(value));
     }
 }
