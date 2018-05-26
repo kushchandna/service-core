235ee47be7f32d9eb561ebe8c89fa013783ce11a
Kush <Kush@KUSH>
2018-05-26 13:33:49 +0530

    minor update

--- utils/src/test/java/com/kush/utils/signaling/sample/remote/Server.java
+++ utils/src/test/java/com/kush/utils/signaling/sample/remote/Server.java
@@ -9,22 +9,11 @@ import com.kush.utils.signaling.sample.signals.SampleMessageSignal;
 
 class Server {
 
-    private final SignalSpace signalSpace;
-
-    public Server(SignalSpace signalSpace) {
-        this.signalSpace = signalSpace;
-    }
-
     public static void main(String[] args) {
         SignalEmitter signalEmitter = SignalEmitters.newAsyncEmitter();
         SignalSpace signalSpace = new SignalSpace(Executors.newSingleThreadExecutor(), signalEmitter);
-        Server server = new Server(signalSpace);
-        server.sendMessageSignal(new SampleMessageSignal("User A", "First Text for User A"));
-        server.sendMessageSignal(new SampleMessageSignal("User B", "First Text for User B"));
-        server.sendMessageSignal(new SampleMessageSignal("User A", "Second Text for User A"));
-    }
-
-    private void sendMessageSignal(SampleMessageSignal messageSignal) {
-        signalSpace.emit(messageSignal);
+        signalSpace.emit(new SampleMessageSignal("User A", "First Text for User A"));
+        signalSpace.emit(new SampleMessageSignal("User B", "First Text for User B"));
+        signalSpace.emit(new SampleMessageSignal("User A", "Second Text for User A"));
     }
 }
