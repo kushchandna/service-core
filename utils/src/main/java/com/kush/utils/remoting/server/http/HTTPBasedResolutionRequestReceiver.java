c4b41fbb793c58922a21b20a33eef92116f0ad64
Kush <Kush@KUSH>
2018-05-26 13:38:55 +0530

    adding jetty server for http remote connections

---
+++ utils/src/main/java/com/kush/utils/remoting/server/http/HTTPBasedResolutionRequestReceiver.java
@@ -0,0 +1,48 @@
+package com.kush.utils.remoting.server.http;
+
+import java.util.concurrent.Executor;
+
+import org.eclipse.jetty.server.Server;
+
+import com.kush.utils.remoting.ResolutionFailedException;
+import com.kush.utils.remoting.Resolvable;
+import com.kush.utils.remoting.server.ResolutionRequest;
+import com.kush.utils.remoting.server.ResolutionRequestsReceiver;
+import com.kush.utils.remoting.server.ShutdownFailedException;
+import com.kush.utils.remoting.server.StartupFailedException;
+
+public class HTTPBasedResolutionRequestReceiver<T extends Resolvable> extends ResolutionRequestsReceiver<T> {
+
+    private final int port;
+
+    private Server server;
+
+    public HTTPBasedResolutionRequestReceiver(Executor requestResolverExecutor, int port) {
+        super(requestResolverExecutor);
+        this.port = port;
+    }
+
+    @Override
+    protected void performStartup() throws StartupFailedException {
+        server = new Server(port);
+        try {
+            server.start();
+        } catch (Exception e) {
+            throw new StartupFailedException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected void performStop() throws ShutdownFailedException {
+        try {
+            server.stop();
+        } catch (Exception e) {
+            throw new ShutdownFailedException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    protected ResolutionRequest getNextResolvableQuery() throws ResolutionFailedException {
+        return null;
+    }
+}
