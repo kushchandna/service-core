df9759b2d276a4002c9481aa3a52f2c44c810d81
Kush Chandna <kushchandna@gmail.com>
2018-05-29 20:13:17 +0530

    creating notifiable client

--- service-client/src/main/java/com/kush/serviceclient/notifications/NotificationHandler.java
+++ service-client/src/main/java/com/kush/serviceclient/notifications/NotificationHandler.java
@@ -1,8 +1,16 @@
 package com.kush.serviceclient.notifications;
 
 import com.kush.lib.service.remoting.notifications.Notification;
+import com.kush.utils.remoting.ResolutionFailedException;
+import com.kush.utils.remoting.server.Resolver;
 
-public interface NotificationHandler {
+public abstract class NotificationHandler implements Resolver<Notification> {
 
-    void handle(Notification notification);
+    @Override
+    public Object resolve(Notification notification) throws ResolutionFailedException {
+        handle(notification);
+        return null;
+    }
+
+    protected abstract void handle(Notification notification);
 }
